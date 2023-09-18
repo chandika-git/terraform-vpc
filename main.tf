0229404cae3cce21902264b7a14011f560571dbf
chandika <chandika@gmail.com>
2023-09-18 17:09:22 +0000

    wordpress - frontend sg created

--- main.tf
+++ main.tf
@@ -146,3 +146,43 @@ resource "aws_security_group" "bastion" {
   }
 }
 
+
+
+resource "aws_security_group" "frontend" {
+  name        = "${var.project_name}-${var.project_env}-frontend"
+  description = "Allow only HTTP & HTTPS port from my all"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "HTTP port"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTPS port"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.project_env}-frontend",
+    Project = var.project_name,
+    Env = var.project_env
+  }
+}
+
