6009bc3e14d51c0e7b454c79135444a65a334106
chandika <chandika@gmail.com>
2023-09-18 17:05:59 +0000

    wordpress - bastion sg created

--- main.tf
+++ main.tf
@@ -115,3 +115,34 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private.id
 }
 
+
+
+resource "aws_security_group" "bastion" {
+  name        = "${var.project_name}-${var.project_env}-bastion"
+  description = "Allow only SSH port from my ip"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "SSH port"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.project_env}-bastion",
+    Project = var.project_name,
+    Env = var.project_env
+  }
+}
+
