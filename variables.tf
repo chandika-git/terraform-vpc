9cc4bc150b56438813226ab67768ab7dc4f338fa
chandika <chandika@gmail.com>
2023-09-18 16:32:15 +0000

    initial commit

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "cidr_block" {
+default = "10.0.0.0/16"
+}
+ 
+ 
+variable "project_name" {
+default = "fb"
+}
+ 
+ 
+variable "project_env" {
+default = "dev"
+}
+ 
+variable "region" {
+default = "ap-south-1"
+}
